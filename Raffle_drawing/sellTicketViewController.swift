043b1cb012f5b1df555d7d092d3d45f796316b69
hoshiuhang <hoshiuhang@gmail.com>
2020-05-24 20:36:02 +1000

    add Searchbtn

---
+++ Raffle_drawing/sellTicketViewController.swift
@@ -0,0 +1,38 @@
+//
+//  sellTicketViewController.swift
+//  Raffle_drawing
+//
+//  Created by Sunny on 24/5/20.
+//  Copyright © 2020 Lindsay Wells. All rights reserved.
+//
+
+import UIKit
+
+class sellTicketViewController: UIViewController {
+
+    @IBAction func searchUserBtn(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let searchBtn = UIButton()
+        searchBtn.layer.cornerRadius = 25.0
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
