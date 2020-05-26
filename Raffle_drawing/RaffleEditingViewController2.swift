a728184c3c78e69b07ef25046eebdbe9c447497b
hoshiuhang <hoshiuhang@gmail.com>
2020-05-27 03:40:04 +1000

    raffle editing connect story board to class

--- Raffle_drawing/RaffleEditingViewController2.swift
+++ Raffle_drawing/RaffleEditingViewController2.swift
@@ -9,51 +9,59 @@
 import UIKit
 
 class RaffleEditingViewController2: UIViewController {
-
+//  create raffle variable ti stroe current raffle information
+    var raffle: Raffle? //store the selected Raffle
 //    Interface element
-    
-    
     @IBOutlet weak var titleField: UITextField!
-    
 //    Price adjust
-    
-    
     @IBOutlet weak var priceLabel: UILabel!
     
+    @IBOutlet weak var priceStepperOutlet: UIStepper!
     
-    @IBAction func stepper(_ sender: UIStepper) {
+    @IBAction func priceStepperControl(_ sender: UIStepper) {
+        priceLabel.text = Int32(sender.value).description
     }
     
     
     
 //  Adjusting Max ticket
     @IBOutlet weak var maxTicketLabel: UILabel!
+
+   
+    @IBOutlet weak var maxTicketOutlet: UISlider!
     
     
-    @IBAction func slider(_ sender: UISlider) {
+    @IBAction func maxTicketSlider(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        maxTicketLabel.text = "\(currentValue)"
     }
-    
-    
     
     @IBOutlet weak var descriptionField: UITextField!
-    
-    
+  
     @IBOutlet weak var prizeField: UITextField!
     
-    
-    
+//  number of winner
     @IBOutlet weak var winnerLabel: UILabel!
+
+    @IBOutlet weak var winnerStepperOutlet: UIStepper!
     
-    @IBAction func winnerStepper(_ sender: UIStepper) {
+    @IBAction func winnerStepperControl(_ sender: UIStepper) {
+        winnerLabel.text = Int32(sender.value).description
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    priceStepperOutlet.wraps = true
+    priceStepperOutlet.autorepeat = true
+    priceStepperOutlet.minimumValue = 1
+    priceStepperOutlet.maximumValue = 100
 
-        // Do any additional setup after loading the view.
+    
+    winnerStepperOutlet.wraps = true
+    winnerStepperOutlet.autorepeat = true
+    winnerStepperOutlet.minimumValue = 1
+    winnerStepperOutlet.maximumValue = 100
     }
     
 
