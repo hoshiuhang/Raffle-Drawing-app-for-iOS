55157393f1d38350972605b9df9df284a97383eb
hoshiuhang <hoshiuhang@gmail.com>
2020-05-28 17:29:16 +1000

    sell ticket check

---
+++ Raffle_drawing/Winner.swift
@@ -0,0 +1,21 @@
+//
+//  Winner.swift
+//  Raffle_drawing
+//
+//  Created by Sunny on 28/5/20.
+//  Copyright © 2020 Lindsay Wells. All rights reserved.
+//
+
+import Foundation
+
+public struct Ticket
+{
+    var ticketID:Int32 = -1 //from ticket table
+    var tPrice:Int32 //from raffle itself
+    var playerName:String //from player table
+    var playerEmail:String
+    var playerContact:Int32
+    var dateTime:String //ticket table
+    
+   
+}
