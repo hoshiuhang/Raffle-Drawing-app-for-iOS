a68b1b74608c6b786df5b064cc9a37482c44b2c8
hoshiuhang <46815762+hoshiuhang@users.noreply.github.com>
2020-05-29 02:22:28 +1000

    Add files via upload
    winning list

---
+++ Raffle_drawing/winnerTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  winnerTableViewCell.swift
+//  Raffle_drawing
+//
+//  Created by Sunny on 28/5/20.
+//  Copyright © 2020 Lindsay Wells. All rights reserved.
+//
+
+import UIKit
+
+class winnerTableViewCell: UITableViewCell {
+
+    
+    
+
+    
+    @IBOutlet weak var ticketIDLabel: UILabel!
+    @IBOutlet weak var playerNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var playerEmailLabel: UILabel!
+    
+    
+    @IBOutlet weak var playerContactLabel: UILabel!
+    
+    
+    @IBOutlet weak var ticketPriceLabel: UILabel!
+    
+    
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
